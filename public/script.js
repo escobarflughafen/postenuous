0de154f73dee2228176dae81cb36ed7dff4a0772
aoi <tachiaoi@pwrbkg4.local>
2020-05-24 04:23:51 +0800

    use table to display comments

--- public/script.js
+++ public/script.js
@@ -1,58 +1,19 @@
-/*$('#input_date').keypress(function(e){ 
-    　　if(e.keyCode=='13'){ 
-    　　　　$.ajax({
-    　　　　　　type: "POST",
-    　　　　　　url: "inquire_date.php", 
-    　　　　　　data: { 
-    　　　　　　　　birth:null,
-     //1.获取用户请求（即某些事件），发送到服务器处理
-    　　　　　　　date:$('#input_date input').val()
-    　　　　　　},
-    　　　　　　dataType: "json",
-    //2.从服务器获取数据
-    　　　　　　success: function(data){
-     　　　　　　　　if (data.success) {
-     　　　　　　　　　　var festival = data.fetivalInquireResult; 
-    //3.将获取的数据载入页面，实现页面事件响应刷新
-    　　　　　　　　　　$('#show_festival').text(festival);
-    　　　　　　　　} else {
-    　　　　　　　　　$('#show_festival').text("获取节日失败")
-    　　　　　　} 
-    　　　},//欢迎加入全栈开发交流群一起学习交流：619586920
-    　　　　　　error: function(jqXHR){     
-      　　　　　　alert("发生错误：" + jqXHR.status);  
-    　　　　　　},     
-    　　　　});
-    　　$('#festival').hide();
-    　　$('#response_festival').show();
-    　　} 
-    });
-*/
-/*
-function autoGrow(element, defaultHeight, limitedHeight) {
-  if (element) {
-    element.style.height = defaultHeight;
-    element.style.height = ((element.scrollHeight >= limitedHeight) ? limitedHeight : element.scrollHeight + "px");
-  }
-}
-*/
-
 function init() {
   let refers = $("a[id^=link-to]");
   for (let i = 0; i < refers.length; i++) {
-      let refer = refers[i]
-      $(refer).text($('#comment-no-' + $(refer).text()).text())
+    let refer = refers[i]
+    $(refer).text($('#comment-no-' + $(refer).text()).text())
   }
   $("a[id^=replyto").click(function () {
-      console.log($(this).prop('href'))
+    console.log($(this).prop('href'))
   });
   $("a[id^=link-to-").click(function () {
-      let replytoCommentElement = $('#' + $(this).prop('href').split('#')[1]);
-      replytoCommentElement.addClass('comment-emphasized');
+    let replytoCommentElement = $('#' + $(this).prop('href').split('#')[1]);
+    replytoCommentElement.addClass('comment-emphasized');
 
-      setTimeout(() => {
-          replytoCommentElement.removeClass('comment-emphasized');
-      }, 1000)
+    setTimeout(() => {
+      replytoCommentElement.removeClass('comment-emphasized');
+    }, 1000)
   });
 }
 
@@ -132,21 +93,14 @@ function showComment(element) {
 
 }
 
-function toggleDisabledCommentsVisibility(element) {
-  if($(element).prop('show-all') == undefined) {
-    $(element).prop('show-all', true)
-  }
+function toggleDisabledCommentsVisibility(element, showAll = true) {
+
+
   let params = {
     isAjax: true,
-    showDisabledComments: $(element).prop('show-all')
+    showDisabledComments: showAll
   }
-  /*
-  if ($(element).prop('show-all')) {
-    $(element).text('hide removed comments');
-  } else {
-    $(element).text('show removed comments');
-  }
-  */
+
   $(element).addClass('disabled')
 
   $.ajax({
@@ -155,15 +109,13 @@ function toggleDisabledCommentsVisibility(element) {
     dateType: 'json',
     data: params
   }).done(function (data) {
-    console.log(data)
     $('#comments-area').html(data.commentsHTML);
     $('#replyto').html(data.replytoOptions);
-    $(element).prop('show-all', !$(element).prop('show-all'));
     $(element).removeClass('disabled')
-    $(element).text(($(element).prop('show-all')) ? 'show disabled comments' : 'hide disabled comments' )
     init()
-  }).fail(() => {
-    alert('BadRequest')
+  }).fail((message) => {
+    console.log(message)
+    alert('BadRequest: ' + message.toString())
     $(element).removeClass('disabled')
   })
 }
